88d8bfaede9a1d8338270de8df9ec5c2d3058f7c
Bao Doan <baodoan@Baos-MacBook-Pro.local>
2019-05-17 19:20:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+process.stdin.on('keypress', (key, data) => {
+  if (data.ctrl && data.name === 't') {
+    process.exit();
+  } else { 
+    console.log('data', data);
+  }
+});
+console.log('Press a key');
